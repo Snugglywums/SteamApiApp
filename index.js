371691ca8e68d1e816a8f939e8314a625ee4d75f
Snugglywums <69602411+Snugglywums@users.noreply.github.com>
2020-09-12 15:23:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // 76561198320255033 my steam ID
 'use strict';
 
-const apiKey = '18A549D8FC139D86AE8D2CB94F218CCF';
+const apiKey = '6D5542C11A7DACE3BEFBD50984D8F723';
 
 function getUserbyId(){
     
     let userId = document.getElementById("mySearch").value;
     console.log(userId);
 
-    fetch(' http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=18A549D8FC139D86AE8D2CB94F218CCF&format=json&input_json={"steamid":'+ userId +'}', )
+    fetch(' http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=6D5542C11A7DACE3BEFBD50984D8F723&format=json&input_json={"steamid":'+ userId +'}', )
     .then(response => response.json())
     .then(responseJson => 
         displayResults(responseJson))
